9c5a893c82c9548ced50d07c4eb229c8a73d6b5e
tasos disorder <anastasios.louka1991@gmail.com>
2023-05-06 14:10:49 +0300

    Implement simple python ex1

---
+++ ex1.py
@@ -0,0 +1,13 @@
+x = int(input("num1 :"))
+y = int(input("num2 :"))
+z = int(input("num3 :"))
+
+if x > y > z :
+    print(x)
+    
+if y > x > z :
+    print(y)
+    
+else :
+    print(z)
+        
