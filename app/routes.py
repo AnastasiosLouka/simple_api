6e2e1b8b181bb0c65a26c58ecf01a1f76445a3e6
AnastasiosLouka <anastasios.louka1991@gmail.com>
2023-05-13 16:45:25 +0300

    Implement forms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from app.forms import LoginForm
+from flask import render_template, flash, redirect, jsonify
 
 @app.route('/')
 @app.route('/index')
@@ -17,7 +18,23 @@ def index():
         }
     ]
     return render_template('index.html',title=title, user=user, posts=posts)
+    # return jsonify({
+    #     "title": title,
+    #     "username": user['username'],
+    #     "posts": posts
+    # }), 200
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+   
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
