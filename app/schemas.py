db55f8064718241ff5e6dcf7ff623c7261588fea
AnastasiosLouka <anastasios.louka1991@gmail.com>
2023-08-06 21:28:00 +0300

    Add id variable

--- app/schemas.py
+++ app/schemas.py
@@ -1,9 +1,11 @@
 from marshmallow import Schema, fields
 
+
 class PostSchema(Schema):
     body = fields.Str()
     user_id = fields.Integer()
     timestamp = fields.Date()
+    id = fields.Integer
 
 
 class UserSchema(Schema):
